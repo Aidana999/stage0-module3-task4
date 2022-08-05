6bc31aa18f7b7f28de15dc064fef46a5b647f16a
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-08-05 21:17:11 +0600

    Update SettingTypes.java

--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-//         first = 10_000_000_000L;
-//         second = 'a';
-//         third = 5 > 9;
-//         forth = 12.90434323324342;
-//         fifth = 12.3F;
-//         sixth = 1;
-//         seventh = 129;
+        long = 10_000_000_000L;
+        char = 'a';
+        int = 5 > 9;
+        double = 12.90434323324342;
+        float = 12.3F;
+        boolean = 1;
+        byte = 129;
     }
 }
